a73140bd5d8cd1ca5fd7ac598e90b19f9c3d5793
Rodrigo Vinicius Alves Carneiro <rvcarneiro@govrj.local>
2016-04-28 15:07:36 -0300

    add do modulo completo no repositório

---
+++ angular-decimal-mask.js
@@ -0,0 +1,37 @@
+angular.module('AngularDecimalMask', [])
+.directive('decimalMask', function () {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModel) {
+
+            element.on('keyup', function(e){
+                var arr = ngModel.$viewValue.replace('.', '').split(""),
+                    newArr = [],
+                    newValue = '';
+                var charErros = 0;
+
+
+                angular.forEach(arr, function(char){
+                    if(isNaN(char) === false) {
+                        newArr.push(char);
+                    }
+                });
+
+                angular.forEach(newArr, function(char, index){
+                    var decimals = 2,
+                        total = newArr.length;
+
+                    if ((total > decimals && (total - decimals) == index)) {
+                        newValue += '.' + char;
+                    } else {
+                        newValue += char;
+                    }
+                });
+
+                ngModel.$setViewValue(newValue);
+                ngModel.$render();
+            });
+        }
+    };
+});
